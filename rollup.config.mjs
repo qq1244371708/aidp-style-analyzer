5d41b1628f29f52d0bbb42c6cc8408890631105f
breeze <gaojun@whalepms.com>
2026-01-30 15:32:34 +0800

    feat(.location): version 0.1.2-beta

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -31,7 +31,9 @@ export default [
     external: externals,
     plugins: [
       resolve({ preferBuiltins: true }),
-      commonjs(),
+      commonjs({
+        ignoreDynamicRequires: true
+      }),
       json()
     ]
   },
@@ -47,7 +49,9 @@ export default [
     external: externals,
     plugins: [
       resolve({ preferBuiltins: true }),
-      commonjs(),
+      commonjs({
+        ignoreDynamicRequires: true
+      }),
       json()
     ]
   }
